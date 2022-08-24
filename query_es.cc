2a280467383aac8da100c405fc9d42c223fd9687
Muhammad Haseeb <mh6218@nyu.edu>
2022-08-24 11:43:32 -0400

    structure for es query

---
+++ query_es.cc
@@ -0,0 +1,6 @@
+#include "query_es.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ query_es.h
@@ -0,0 +1,5 @@
+#include "common.h"
+#include "nlohmann/json.hpp"
+#include <curl/curl.h>
+#include <unordered_map>
+#include <future>
